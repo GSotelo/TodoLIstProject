820738fa0dd79eb81aa6ac2b82e8bb3f078f0f38
Gerax <gsotelo23@gmail.com>
2017-08-22 23:13:51 -0500

    first commit

---
+++ assets/js/todos.js
@@ -0,0 +1,21 @@
+$('ul').on("click", "li", function() {
+	$(this).toggleClass("completed");
+});
+
+$("ul").on("click", "span", function(event) {
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+	if(event.which === 13) {
+		//grab new todo text from input
+		var todoText = $(this).val();
+		//create a new li and add to ul
+		var liStart = "<li><span><i class='fa fa-trash'></i></span> ";
+		$("ul").append(liStart + todoText + "</li>");
+		$(this).val("");
+	}
+});
